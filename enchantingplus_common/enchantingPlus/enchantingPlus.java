d65347292d098b17a6f2081b327a0f96f5cafb49
Judd Morrison <juddm8@msn.com>
2013-04-19 17:14:33 +1000

    Pahimar's Lets Mod Episode 5

---
+++ enchantingplus_common/enchantingPlus/enchantingPlus.java
@@ -0,0 +1,31 @@
+package enchantingPlus;
+
+import enchantingPlus.lib.reference;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = reference.MOD_ID, name = reference.MOD_NAME, version = reference.VERSION)
+
+public class enchantingPlus {
+    
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event) {
+        
+    }
+    
+    @Init
+    public void init(FMLInitializationEvent event) {
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event) {
+        
+    }
+}
---
+++ enchantingplus_common/enchantingPlus/lib/reference.java
@@ -0,0 +1,9 @@
+package enchantingPlus.lib;
+
+public class reference {
+    
+    public static final String MOD_ID = "EP";
+    public static final String MOD_NAME = "Enchanting Plus";
+    public static final String VERSION = "0.0.1";
+
+}
